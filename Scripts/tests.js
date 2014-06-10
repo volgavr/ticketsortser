35b52f60fea9af3b04287c90915fdc0d6f146ef1
volgavr <vol-gavr@yandex.ru>
2014-06-11 00:32:21 +0400

    Created

---
+++ Scripts/tests.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="qunit.js" />
+/// <reference path="tripSorter.js" />
+test("Types of tickets", function () {
+    var a = new TS.TrainTicket(75, "C", "D", "Указывает проводник.");
+    var b = new TS.Ticket("D", "E", "bus");
+    
+    ok(a.hasOwnProperty('seat'), "a.hasOwnProperty('seat')");
+    ok(a.start == "C", "a.start == 'C'");
+    ok(a.hasOwnProperty('start'), "a.hasOwnProperty('start')");
+    ok(b instanceof TS.Ticket, "b instanceof TS.Ticket");
+    ok(a instanceof TS.TrainTicket, "a instanceof TS.TrainTicket");
+    ok(a instanceof TS.Ticket, "a instanceof TS.Ticket");
+    //equal(a.toLocaleString(), "a.toLocaleString()");
+});
+
+test("Sort test, mixed types", function () {
+    var list = [
+        new TS.FlightTicket("SK455", "Gerona Airport", "Stockholm", "3A", 344, "45B"),
+        new TS.Ticket("Barcelona", "Gerona Airport", "airport bus"),
+        new TS.FlightTicket("SK22", "Stockholm", "New York JFK", "7B", "", 22),
+        new TS.TrainTicket("78A", "Madrid", "Barcelona", "45B"),
+        new TS.Ticket("Talavera de la Reina", "Madrid", "bus")
+    ];
+
+    var exp = [
+        new TS.Ticket("Talavera de la Reina", "Madrid", "bus"),
+        new TS.TrainTicket("78A", "Madrid", "Barcelona", "45B"),
+        new TS.Ticket("Barcelona", "Gerona Airport", "airport bus"),
+        new TS.FlightTicket("SK455", "Gerona Airport", "Stockholm", "3A", 344, "45B"),
+        new TS.FlightTicket("SK22", "Stockholm", "New York JFK", "7B", "", 22)
+    ];
+    
+    same(list.arrange(), exp, "Number of elements: " + exp.length);
+});
+
+test("Sort test, toLocaleString()", function () {
+    var list = [
+        new TS.FlightTicket("SK455", "Gerona Airport", "Stockholm", "3A", 344, "45B"),
+        new TS.Ticket("Barcelona", "Gerona Airport", "airport bus"),
+        new TS.FlightTicket("SK22", "Stockholm", "New York JFK", "7B", "", 22),
+        new TS.TrainTicket("78A", "Madrid", "Barcelona", "45B"),
+        new TS.Ticket("Talavera de la Reina", "Madrid", "bus 416")
+    ];
+
+    var exp = [
+        "Take the bus 416 from Talavera de la Reina to Madrid. No seat assignment.",
+        "Take train 78A from Madrid to Barcelona. Seat 45B.",
+        "Take the airport bus from Barcelona to Gerona Airport. No seat assignment.",
+        "From Gerona Airport, take flight SK455 to Stockholm. Gate 45B. Seat 3A. Baggage drop at ticket counter 344.",
+        "From Stockholm, take flight SK22 to New York JFK. Gate 22. Seat 7B. Baggage will be automatically transferred from your last leg."
+    ];
+    
+    //для иллюстрации входного массива
+    //same(list, list, "Input:")
+    
+    list.arrange();
+    var res = [];
+    for (var i = 0; i < list.length; i++) {
+        res[i] = list[i].toLocaleString();
+    }
+
+    same(res, exp, "After sort and toLocaleString() applying:");
+    
+});
---
+++ Scripts/tripSorter.js
@@ -0,0 +1,170 @@
+﻿/**
+ * tripSorter - A JavaScript Module
+ *
+ * Copyright (c) 2014 Vladimir Gavrilov
+ */
+/**
+* Билет, карточка путешественника
+* Ticket {
+    start - пункт отправления (строка),
+    end - пункт назначения (строка),
+    transport - транспорт (строка), например: автобус 416
+} 
+*/
+
+(function () {
+    this.TS = this.TS || {};
+    var ns = this.TS;
+    /** Constructor.
+      * @param startPoint string
+      * @param endPoint string
+      * @param transport string
+      */
+    ns.Ticket = function (from, to, transport) {
+        this.start = from;
+        this.end = to;
+        this.transport = transport ? transport : 'transport';
+    };
+
+    ns.Ticket.prototype.toLocaleString = function () {
+        return 'Take the ' + this.transport + ' from ' + this.start + ' to ' + this.end + '. No seat assignment.';
+    };
+
+/**
+      * Arranges an array of Ticket objects, browsing 'start' and 'end' props.
+      * @param pos1 int
+      * @param pos2 int
+      */
+Array.prototype.arrange = function () {
+    var a = this,
+        k = 1;
+    
+    while (k < a.length) {
+        
+        for (var i = k; i < a.length; i++) {
+            if (a[i].start == a[k-1].end) {
+                a.move(i, k);
+                k++;
+            }
+
+            if (a[i].end == a[0].start) {
+                a.move(i, 0);
+                k++;
+
+            }
+
+        }
+
+    }
+
+    return a;
+}
+
+//ancillary method for moving an element from one pos. to another
+//Array.prototype.move = function (from, to) {
+//    this.splice(to, 0, this.splice(from, 1)[0]);
+//};
+
+/**
+  * Moves an element from pos1 to pos2 within the array.
+  * @param pos1 int
+  * @param pos2 int
+  */
+Array.prototype.move = function (pos1, pos2) {
+    // local variables
+    var i, tmp;
+    // cast input parameters to integers
+    pos1 = parseInt(pos1, 10);
+    pos2 = parseInt(pos2, 10);
+    // if positions are different and inside array
+    if (pos1 !== pos2 && 0 <= pos1 && pos1 <= this.length && 0 <= pos2 && pos2 <= this.length) {
+        // save element from position 1
+        tmp = this[pos1];
+        // move element down and shift other elements up
+        if (pos1 < pos2) {
+            for (i = pos1; i < pos2; i++) {
+                this[i] = this[i+1];
+            }
+        }
+            // move element up and shift other elements down
+        else {
+            for (i = pos1; i > pos2; i--) {
+                this[i] = this[i-1];
+            }
+        }
+        // put element from position 1 to destination
+        this[pos2] = tmp;
+    }
+}
+
+})();
+
+//TS.TrainTicket = function (start, end, number, seat) {
+//    TS.Ticket.call(this, start, end);
+//    this.number = number;
+//    this.seat = seat;
+//};
+//TS.TrainTicket.prototype.toLocaleString = function () {
+//    return 'Take train ' + this.number + ' from ' + this.start + ' to ' + this.end + '. Seat ' + this.seat;
+//}
+
+/**
+*   Добавляем билет на поезд (TrainTicket), наследуемся от Ticket
+*/
+
+TS.TrainTicket = (function (parent) {
+    trainTicket.prototype = new TS.Ticket();
+    trainTicket.prototype.constructor = trainTicket;
+
+    /** Constructor.
+      * @param number string
+      * @param from string
+      * @param to string
+      * @param seat string
+      */
+    function trainTicket(number, from, to, seat) {
+        parent.call(this, from, to);
+        this.number = number;
+        this.seat = seat;
+    }
+
+    trainTicket.prototype.toLocaleString = function () {
+        return 'Take train ' + this.number + ' from ' + this.start + ' to ' + this.end + '. Seat ' + this.seat + '.';
+    }
+
+    return trainTicket;
+})(TS.Ticket);
+
+/**
+*   Добавляем билет на самолет (FlightTicket), наследуемся от Ticket
+*/
+TS.FlightTicket = (function (parent) {
+    flightTicket.prototype = new TS.Ticket();
+    flightTicket.prototype.constructor = flightTicket;
+
+    /** Constructor.
+      * @param number string
+      * @param from string
+      * @param to string
+      * @param seat string
+      * @param buggageDropOff string
+      * @param gate string
+      */
+    function flightTicket(number, from, to, seat, buggageDropOff, gate) {
+        parent.call(this, from, to);
+        this.number = number;
+        this.seat = seat;
+        this.buggageDropOff = buggageDropOff;
+        this.gate = gate;
+    }
+
+    flightTicket.prototype.toLocaleString = function () {
+        return ('From ' + this.start + ', take flight ' + this.number + ' to ' + this.end + '.'
+            + (this.gate ? ' Gate ' + this.gate + '.' : '')
+            + ' Seat ' + this.seat + '.'
+            + (this.buggageDropOff ? ' Baggage drop at ticket counter ' + this.buggageDropOff + '.' : ' Baggage will be automatically transferred from your last leg.')
+        );
+    };
+
+    return flightTicket;
+})(TS.Ticket);
